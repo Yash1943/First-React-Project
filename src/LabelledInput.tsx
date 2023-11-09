6c005fb02c49f0d8913ab86bdffeb62be3c478e0
Yash1943 <joshiyash1942003@gmail.com>
2023-11-09 12:10:17 +0530

    State and Hook at glance

--- src/LabelledInput.tsx
+++ src/LabelledInput.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 
-export default function LabelledInput(props: { label: string; fieldType: string }) {
+export default function LabelledInput(props: {
+  id: number;
+  label: string;
+  fieldType: string;
+  removeFieldCB: (id: number) => void;
+}) {
   return (
     <>
       <label>{props.label}</label>
-      <input
-        className="border-2 border-gray-200 rounded-lg p-2 my-2 w-full"
-        type={props.fieldType}
-      />
+      <div className="flex gap-2">
+        <input
+          className="border-2 border-gray-200 rounded-lg p-2 my-2 flex-1"
+          type={props.fieldType}
+        />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-lg cursor-pointer"
+          onClick={(_) => props.removeFieldCB(props.id)}>
+          Remove Field
+        </button>
+      </div>
     </>
   );
 }
